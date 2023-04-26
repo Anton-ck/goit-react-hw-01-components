8a78f3d752f59db6dce31c008b9cfe8822123119
Anton Nikolaenko <anton.ck.ua@gmail.com>
2023-04-26 18:20:47 +0300

    first task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { Profile } from './profile/Profile';
+import user from './profile/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+      <Profile avatar={user.avatar} username={user.username} tag={user.tag} location={user.location}
+        followers={user.stats.followers} views={user.stats.views} likes={user.stats.likes} />
+
   );
 };
